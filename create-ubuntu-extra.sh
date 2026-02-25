400c3fad22c5619e1bbea6555f8655820083f21d
sig9 <sig9@sig9.org>
2026-02-26 08:07:47 +0900

    Modified create-ubuntu-extra.sh to support Ubuntu 24 as of February 25, 2026

--- create-ubuntu-extra.sh
+++ create-ubuntu-extra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="file"
-DATE="20260217"
+DATE="20260225"
 RESIZE="+1.5G"
 SCRIPT="https://raw.githubusercontent.com/sig9org/cml-definitions/master/definitions/ubuntu-extra/script/ubuntu-extra.sh"
 
