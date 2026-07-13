4fb2ab23b36dd589a68fa0e2e64ba6da1184fb9b
sig9org <sig9@sig9.org>
2026-07-13 10:15:32 +0900

    Add zebra-rs node, image, and custom build definitions
    Add zebra-rs routing stack support to CML:
    - Node definition (Ubuntu cloud-init, router nature, 8 interfaces)
    - Image definition for version 26.7.4
    - Custom image build script (create-custom-zebra-rs.sh)
    - In-image customization script (zebra-rs install, SSH, syslog)
    - Taskfile tasks: zebra-rs, zebra-rs-build, zebra-rs-latest

---
+++ create-custom-zebra-rs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DIR="file"
+CODENAME="resolute"
+DATE=$1
+RESIZE="+1.5G"
+SCRIPT="https://raw.githubusercontent.com/sig9org/cml-definitions/master/definitions/zebra-rs/script/custom.sh"
+ZEBRA_VERSION=$2
+
+echo "############################################################"
+echo "##### (1/x) Download the image and make a copy for work.."
+echo "############################################################"
+
+mkdir -p ${DIR}/
+rm -f ${DIR}/${CODENAME}-server-cloudimg-amd64.img
+axel https://cloud-images.ubuntu.com/${CODENAME}/${DATE}/${CODENAME}-server-cloudimg-amd64.img \
+  -o ${DIR}/${CODENAME}-server-cloudimg-amd64.qcow2
+cp ${DIR}/${CODENAME}-server-cloudimg-amd64.qcow2 \
+  ${DIR}/${CODENAME}-server-cloudimg-amd64-tmp.qcow2
+eza -hl file/*.qcow2
+
+echo "############################################################"
+echo "##### (2/x) Resize the image."
+echo "############################################################"
+
+qemu-img resize ${DIR}/${CODENAME}-server-cloudimg-amd64-tmp.qcow2 ${RESIZE}
+qemu-img info ${DIR}/${CODENAME}-server-cloudimg-amd64-tmp.qcow2
+
+echo "############################################################"
+echo "##### (3/x) Customize the image."
+echo "############################################################"
+
+virt-customize -v -x -a ${DIR}/${CODENAME}-server-cloudimg-amd64-tmp.qcow2 \
+  --run-command 'growpart /dev/sda 1' \
+  --run-command 'resize2fs /dev/sda1' \
+  --run-command "curl -Ls ${SCRIPT} | bash -s ${ZEBRA_VERSION}"
+
+echo "############################################################"
+echo "##### (4/x) Compress images."
+echo "############################################################"
+
+virt-sparsify --compress \
+  ${DIR}/${CODENAME}-server-cloudimg-amd64-tmp.qcow2 \
+  ${DIR}/zebra-rs-${ZEBRA_VERSION}.qcow2
+eza -hl file/*.qcow2
+rm -f ${DIR}/${CODENAME}-server-cloudimg-amd64-tmp.qcow2
---
+++ definitions/zebra-rs/script/custom.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+ZEBRA_VERSION=$1
+
+# Install zebra-rs
+install -d -m 0755 /etc/apt/keyrings
+curl -fsSL https://zebra.rs/apt/zebra-rs-archive-keyring.asc | sudo tee /etc/apt/keyrings/zebra-rs.asc >/dev/null
+printf 'Types: deb\nURIs: %s\nSuites: ./\nSigned-By: %s\n' \
+  "https://github.com/zebra-rs/zebra-rs.github.io/releases/download/apt-resolute" "/etc/apt/keyrings/zebra-rs.asc" \
+  | sudo tee /etc/apt/sources.list.d/zebra-rs.sources >/dev/null
+rm -f /etc/apt/sources.list.d/zebra-rs.list
+apt-get update
+apt-get install -y --allow-downgrades zebra-rs=${ZEBRA_VERSION}
+
+useradd -m -g zebra-rs zebra
+echo "zebra:zebra" | chpasswd
+echo 'exec /usr/bin/vty' | tee -a /home/zebra/.bashrc
+
+# Disable AppArmor
+systemctl stop apparmor.service
+systemctl disable apparmor.service
+
+# Timezone & NTP
+timedatectl set-timezone Asia/Tokyo
+cat << EOF >> /etc/systemd/timesyncd.conf
+NTP=162.159.200.123 162.159.200.1
+EOF
+
+# SSH
+sed -i -e "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sed -i -e "s/#ClientAliveInterval 0/ClientAliveInterval 60/g" /etc/ssh/sshd_config
+sed -i -e "s/#ClientAliveCountMax 3/ClientAliveCountMax 5/g" /etc/ssh/sshd_config
+
+cat << EOF > /etc/ssh/ssh_config.d/99_lab.conf
+KexAlgorithms +diffie-hellman-group1-sha1
+Ciphers aes128-cbc,aes256-ctr
+StrictHostKeyChecking no
+UserKnownHostsFile=/dev/null
+EOF
+
+# Enable "/var/log/messages"
+cat << 'EOF' > /etc/rsyslog.d/50-default.conf
+#  Default rules for rsyslog.
+#
+#   For more information see rsyslog.conf(5) and /etc/rsyslog.conf
+
+#
+# First some standard log files.  Log by facility.
+#
+auth,authpriv.*   /var/log/auth.log
+*.*;auth,authpriv.none  -/var/log/syslog
+#cron.*    /var/log/cron.log
+#daemon.*   -/var/log/daemon.log
+kern.*    -/var/log/kern.log
+#lpr.*    -/var/log/lpr.log
+mail.*    -/var/log/mail.log
+#user.*    -/var/log/user.log
+
+#
+# Logging for the mail system.  Split it up so that
+# it is easy to write scripts to parse these files.
+#
+#mail.info   -/var/log/mail.info
+#mail.warn   -/var/log/mail.warn
+mail.err   /var/log/mail.err
+
+#
+# Some "catch-all" log files.
+#
+#*.=debug;\
+# auth,authpriv.none;\
+# news.none;mail.none -/var/log/debug
+*.=info;*.=notice;*.=warn;\
+ auth,authpriv.none;\
+ cron,daemon.none;\
+ mail,news.none  -/var/log/messages
+
+#
+# Emergencies are sent to everybody logged in.
+#
+*.emerg    :omusrmsg:*
+
+#
+# I like to have messages displayed on the console, but only on a virtual
+# console I usually leave idle.
+#
+#daemon,mail.*;\
+# news.=crit;news.=err;news.=notice;\
+# *.=debug;*.=info;\
+# *.=notice;*.=warn /dev/tty8
+EOF
+
+cat << 'EOF' > /etc/logrotate.d/syslog
+/var/log/messages
+{
+ rotate 4
+ weekly
+ missingok
+ notifempty
+ compress
+ delaycompress
+ sharedscripts
+ postrotate
+  /usr/lib/rsyslog/rsyslog-rotate
+ endscript
+}
+EOF
+
+# Install uncmnt
+curl -L https://github.com/sig9org/uncmnt/releases/download/v0.0.2/uncmnt_v0.0.2_linux_amd64 -o /usr/local/bin/uncmnt && \
+chmod 755 /usr/local/bin/uncmnt
+
+# Clean up
+apt clean all
+apt -y autoremove
+history -c
